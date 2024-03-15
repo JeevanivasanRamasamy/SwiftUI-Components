67091e3e16dc95ed875cf3a14074faa3d9a8a805
jeeva.rk <jeevanivasanramasamy@gmail.com>
2024-03-15 23:18:34 +0530

    Initial commit --base project created

---
+++ SwiftUI Components/SwiftUI_ComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ComponentsApp.swift
+//  SwiftUI Components
+//
+//  Created by jeeva-14880 on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
